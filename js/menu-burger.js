e29abe0d43436a7d04b09f90e0b11281ac9d32f7
Raquel <raquelsfolio@gmail.com>
2021-03-05 17:36:00 +0100

    Created navigation hamburguer menu for the website

--- js/menu-burger.js
+++ js/menu-burger.js
@@ -1,5 +1,10 @@
-const menuBtn =document.querySelector('.menu-btn');
+const menuBtn = document.querySelector('.menu-btn');
 const burger = document.querySelector('.menu-btn__burger');
+
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+
+const navItems = document.querySelectorAll('.menu-nav__item');
 
 
 menuBtn.addEventListener('click',toggleMenu);
@@ -9,10 +14,19 @@ let showMenu = false;
 function toggleMenu(){
     if(!showMenu){
         burger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+
+        navItems.forEach ( item => item.classList.add('open'));
+        
 
         showMenu = true;
     }else{
         burger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+
+         navItems.forEach ( item => item.classList.remove('open'));
 
         showMenu = false;
     }
